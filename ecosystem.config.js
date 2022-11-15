b7c867b0966c0c4be9b86715ba4d387c087b6b6f
Alexander Bentkamp <bentkamp@gmail.com>
2022-11-15 16:58:26 +0100

    set 8001 as pm2 port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,7 @@ module.exports = {
     script : "server/index.js",
     env: {
        NODE_ENV: "production",
-       SSL_CRT_FILE: "~/adam_math_uni-duesseldorf_de_cert.cer",
-       SSL_KEY_FILE: "~/private_ssl_key.pem"
+       PORT: 8001
     },
   }]
 }
