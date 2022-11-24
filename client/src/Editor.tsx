fb240d785dfeb7189f6033f035b7caa48cc26221
Alexander Bentkamp <bentkamp@gmail.com>
2022-11-24 10:14:05 +0100

    monaco setup file

--- client/src/Editor.tsx
+++ client/src/Editor.tsx
@@ -21,60 +21,11 @@ import * as lightPlusTheme from './lightPlus.json'
 import * as leanSyntax from './syntaxes/lean.json'
 import * as leanMarkdownSyntax from './syntaxes/lean-markdown.json'
 import * as codeblockSyntax from './syntaxes/codeblock.json'
+import { monacoSetup } from './monacoSetup'
 
 const socketUrl = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/websocket"
 
-// map of monaco "language id's" to TextMate scopeNames
-const grammars = new Map()
-grammars.set('lean4', 'source.lean')
-
-monaco.editor.defineTheme('vs-code-theme-converted', lightPlusTheme as any);
-
-// register Monaco languages
-monaco.languages.register({
-  id: 'lean4',
-  extensions: ['.lean']
-})
-
-let config: any = { ...languageConfig }
-config.autoClosingPairs = config.autoClosingPairs.map(
-  pair => { return {'open': pair[0], 'close': pair[1]} }
-)
-monaco.languages.setLanguageConfiguration('lean4', config);
-
-const registry = new Registry({
-  getGrammarDefinition: async (scopeName) => {
-    if (scopeName === 'source.lean') {
-      return {
-          format: 'json',
-          content: JSON.stringify(leanSyntax)
-      }
-    } else if (scopeName === 'source.lean.markdown') {
-      return {
-          format: 'json',
-          content: JSON.stringify(leanMarkdownSyntax)
-      }
-    } else {
-      return {
-          format: 'json',
-          content: JSON.stringify(codeblockSyntax)
-      }
-    }
-  }
-});
-
-// Load onigasm
-(async () => {
-  try {
-    await loadWASM('./onigasm.wasm')
-  } catch (err) {
-    // Hot module replacement can cause us to run this code twice and that's ok.
-    if (!(err as Error).message?.startsWith('Onigasm#init has been called')) {
-      throw err
-    }
-  }
-  wireTmGrammars(monaco, registry, grammars)
-})()
+monacoSetup()
 
 const Editor: React.FC<{setRestart?, onDidChangeContent?, value: string}> =
     ({setRestart, onDidChangeContent, value}) => {
