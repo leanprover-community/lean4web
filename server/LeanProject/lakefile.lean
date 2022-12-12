e115cfd02718b47fe08fcf159f3a6fed9f0ce179
Jon Eugster <Jon.Eugster@hhu.de>
2022-12-12 09:35:25 +0100

    update lakefile.lean to require mathlib master

--- server/LeanProject/lakefile.lean
+++ server/LeanProject/lakefile.lean
@@ -6,7 +6,7 @@ package leanProject {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
+  "https://github.com/leanprover-community/mathlib4"@"master"
 
 @[default_target]
 lean_lib LeanProject {
