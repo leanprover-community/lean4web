46b56f06cc94d6ba63e9a7ccc88e34ea2b1f64f7
Sebastian Ullrich <sebasti@nullri.ch>
2023-10-09 18:03:05 +0200

    update text

--- client/src/config/text.tsx
+++ client/src/config/text.tsx
@@ -5,14 +5,6 @@ import React from "react"
  * contactInformation: React fragment like `<>blah blah</>` to be inserted inside a `<p>`. Alternatively `null` to be ommitted.
  */
 export const text = {
-  serverCountry: 'Germany',
-  contactInformation: <>
-      <a href="https://www.math.hhu.de/lehrstuehle-/-personen-/-ansprechpartner/innen/lehrstuehle-des-mathematischen-instituts/lehrstuhl-fuer-algebraische-geometrie/team/dr-alexander-bentkamp">Alexander Bentkamp</a>,&nbsp;
-      <a href="https://www.math.hhu.de/lehrstuehle-/-personen-/-ansprechpartner/innen/lehrstuehle-des-mathematischen-instituts/lehrstuhl-fuer-algebraische-geometrie/team/jon-eugster">Jon Eugster</a><br />
-      Mathematisches Institut der Heinrich-Heine-Universität Düsseldorf<br />
-      Universitätsstr. 1<br />
-      40225 Düsseldorf<br />
-      Germany<br />
-      +49 211 81-12173<br />
-    </>
+  serverCountry: 'Finland',
+  contactInformation: null
 }
