baf6e34eccf44fd1be1bfda1f32230fc61238dc1
Sebastian Ullrich <sebasti@nullri.ch>
2023-10-10 11:49:58 +0200

    further harden fs sandbox

--- server/bubblewrap.sh
+++ server/bubblewrap.sh
@@ -1,16 +1,18 @@
 #/bin/bash
 
-ELAN_HOME=/home/lean4web/.elan
+LEAN_ROOT=$(cd LeanProject; lean --print-prefix)
+LEAN_PATH=$(cd LeanProject; lake env printenv LEAN_PATH)
 
 (exec bwrap\
   --ro-bind ./LeanProject /LeanProject \
-  --ro-bind $ELAN_HOME /elan \
-  --ro-bind /nix /nix \
+  --ro-bind $LEAN_ROOT /lean \
+  --ro-bind $GLIBC $GLIBC `# only dep of bin/lean` \
   --dev /dev \
   --proc /proc \
   --clearenv \
-  --setenv PATH "/elan/bin:$PATH" \
-  --setenv ELAN_HOME "/elan" \
+  --setenv PATH "/lean/bin" \
+  --setenv LAKE "/no" `# tries to invoke git otherwise` \
+  --setenv LEAN_PATH "$LEAN_PATH" \
   --unshare-user \
   --unshare-pid  \
   --unshare-net  \
