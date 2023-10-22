0a475a7dec3030f4cc5efe1f3a3227184fb549e0
Alexander Bentkamp <bentkamp@gmail.com>
2023-10-22 10:22:41 +0200

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       targets: [
         {
           src: 'node_modules/@leanprover/infoview/dist/*.production.min.js',
-          dest: '/'
+          dest: '.'
         }
       ]
     })],
