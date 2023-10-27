783bc6cf6a64a688e0c0e14e58556c237b729fd0
joneugster <eugster.jon@gmail.com>
2023-10-27 13:28:31 +0200

    get logo working

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-import path from "path-browserify"
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
     viteStaticCopy({
       targets: [
         {
