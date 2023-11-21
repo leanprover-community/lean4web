c21db2d9fe861a6d4945c4c2a654d6d0a5970596
Alexander Bentkamp <bentkamp@gmail.com>
2023-11-21 11:26:48 +0100

    fix abbrev bug

--- client/src/editor/abbreviation/AbbreviationProvider.ts
+++ client/src/editor/abbreviation/AbbreviationProvider.ts
@@ -3,7 +3,7 @@
 import { computed } from 'mobx';
 // import { Disposable } from 'vscode';
 // import { autorunDisposable } from '../utils/autorunDisposable';
-import * as abbreviations from 'lean4/src/abbreviation/abbreviations.json';
+import abbreviations from 'lean4/src/abbreviation/abbreviations.json';
 import { config } from '../../config/config';
 
 export interface SymbolsByAbbreviation {
