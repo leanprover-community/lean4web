ef2f1b72c3750c255d5e87474dc56bae59a24de1
Jon Eugster <eugster.jon@gmail.com>
2023-12-07 18:02:28 +0100

    removing lake-manifest should be superfluous

--- server/build.sh
+++ server/build.sh
@@ -10,16 +10,12 @@ cd $(dirname $0)
 # https://github.com/leanprover-community/mathlib4/wiki/Using-mathlib4-as-a-dependency#updating-mathlib4
 # Additionally, we had once problems with the `lake-manifest` when a new dependency got added
 # to `mathlib`, therefore we now delete it every time for good measure.
-(cd LeanProject &&
-  rm -f ./lake-manifest.json &&
+( cd LeanProject &&
   lake update -R &&
   lake build)
 
   # note: mathlib has now a post-update hook that modifies the `lean-toolchain`
   # and calls `lake exe cache get`. Therefore these two steps are currently superfluous.
-
-# Copy info about new versions to the client.
-# ./copy_versions.sh
 
 duration=$SECONDS
 echo "Finished in $(($duration / 60)):$(($duration % 60)) min."
