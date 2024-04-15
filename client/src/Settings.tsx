278c481ed868ca9eddf809f732623b813cea6b8b
Joachim Breitner <mail@joachim-breitner.de>
2024-04-15 17:03:55 +0200

    Disable other projects than MathlibLatest for now

--- client/src/Settings.tsx
+++ client/src/Settings.tsx
@@ -142,9 +142,7 @@ const Settings: React.FC<{closeNav, theme, setTheme, project, setProject}> =
                     setProject(ev.target.value)
                     console.log(`set Lean project to: ${ev.target.value}`)
                     }} >
-                <option value="plain">Stable Lean</option>
                 <option value="MathlibLatest">Latest Mathlib</option>
-                <option value="DuperDemo">Latest Duper</option>
               </select>
             </p>
 
