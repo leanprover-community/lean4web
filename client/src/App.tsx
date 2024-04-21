714448248b83d1666ca24a0fa5c23fdcbee59834
Andreas Tzionis <andy0n7@gmail.com>
2024-04-21 21:17:59 +0300

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { useEffect, useRef, useState } from 'react'
+
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
+
 import { loadRenderInfoview } from '@leanprover/infoview/loader'
 import { InfoviewApi } from '@leanprover/infoview-api'
 import { InfoProvider } from './infoview'
@@ -14,20 +16,12 @@ const project = 'MathlibLatest'
 
 const code = '#eval 3+1 \n #eval IO.println "hello" \n'
 
-class DisposingWebSocketMessageReader extends WebSocketMessageReader {
-    dispose() {
-      super.dispose();
-      this.socket.dispose();
-    }
-}
-
 monaco.languages.register({
   id: 'lean4',
   extensions: ['.lean']
 })
 
 const Editor: React.FC = () => {
-  const uri = monaco.Uri.parse(`file:///project/${project}.lean`)
   const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null)
   const [infoviewApi, setInfoviewApi] = useState<InfoviewApi | null>(null)
   const [infoProvider, setInfoProvider] = useState<InfoProvider | null>(null)
@@ -35,16 +29,10 @@ const Editor: React.FC = () => {
   const infoviewRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const model = monaco.editor.createModel(code ?? '', 'lean4', uri)
+    const model = monaco.editor.createModel(code, 'lean4')
     const editor = monaco.editor.create(codeviewRef.current!, { model, })
     setEditor(editor)
-    return () => {
-      editor.dispose();
-      model.dispose();
-    }
-  }, [])
 
-  useEffect(() => {
     const socketUrl = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/websocket" + "/" + project
     console.log(`socket url: ${socketUrl}`)
 
@@ -55,7 +43,7 @@ const Editor: React.FC = () => {
           const websocket = new WebSocket(socketUrl)
           websocket.addEventListener('open', () => {
             const socket = toSocket(websocket)
-            const reader = new DisposingWebSocketMessageReader(socket)
+            const reader = new WebSocketMessageReader(socket)
             const writer = new WebSocketMessageWriter(socket)
             resolve({ reader, writer })
           })
@@ -64,7 +52,7 @@ const Editor: React.FC = () => {
     }
 
     // Following `vscode-lean4/webview/index.ts`
-    const client = new LeanClient(connectionProvider, showRestartMessage)
+    const client = new LeanClient(connectionProvider, () => {})
     const infoProvider = new InfoProvider(client)
     const div: HTMLElement = infoviewRef.current!
     const imports = {
@@ -77,15 +65,8 @@ const Editor: React.FC = () => {
     loadRenderInfoview(imports, [infoProvider.getApi(), div], setInfoviewApi)
     setInfoProvider(infoProvider)
     client.restart(project)
-    return () => {
-      infoProvider.dispose();
-      client.dispose();
-    }
-  }, [project])
 
-  const showRestartMessage = () => {
-    // setRestartMessage(true)
-  }
+  }, [])
 
   useEffect(() => {
     if (infoProvider !== null && editor !== null && infoviewApi !== null) {
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
 
-import App from './App';
+import App from './App'
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<React.StrictMode><App /></React.StrictMode>)
+
