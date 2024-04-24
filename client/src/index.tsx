2d154ef10b05cbc9bbfbd5453a323402500f25bd
Jon Eugster <eugster.jon@gmail.com>
2024-04-24 17:54:08 +0200

    change to JuliaMono

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import './css/index.css';
 import App from './App';
+// import './fonts/JuliaMono-Regular.ttf';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
