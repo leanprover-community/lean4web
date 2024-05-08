124ebe51fd374cf5c928acef3bf7e96a062a88df
Joachim Breitner <mail@joachim-breitner.de>
2024-05-08 16:03:58 +0200

    Add latest lean stable

---
+++ Projects/lean-stable/lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package leanstable {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_lib LeanStable {
+  -- add library configuration options here
+}
