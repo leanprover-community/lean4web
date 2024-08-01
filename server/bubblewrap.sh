2bdf616b46e1cb87f91d6eeb17e36a39af7b71dc
Jon Eugster <eugster.jon@gmail.com>
2024-08-01 19:56:46 +0200

    revert changes to bubblewrap.sh

--- server/bubblewrap.sh
+++ server/bubblewrap.sh
@@ -1,28 +1,26 @@
 #/bin/bash
 
-LEAN_ROOT="$(cd $1 && lean --print-prefix)"
-LEAN_PATH="$(cd $1 && lake env printenv LEAN_PATH)"
-GLIBC_PATH="$(nix-store --query "$(patchelf --print-interpreter "$LEAN_ROOT/bin/lean")")"
-
-# print commands as they are executed
-set -x
+ELAN_HOME=$(cd $1 && lake env printenv ELAN_HOME)
 
 (exec bwrap\
-  --ro-bind "$1" /project \
-  --ro-bind "$LEAN_ROOT" /lean \
-  --ro-bind "$GLIBC_PATH" "$GLIBC_PATH" `# only dep of bin/lean` \
+  --bind $1 /project \
+  --bind $ELAN_HOME /elan \
+  --bind /usr /usr \
   --dev /dev \
   --proc /proc \
+  --symlink usr/lib /lib\
+  --symlink usr/lib64 /lib64\
+  --symlink usr/bin /bin\
+  --symlink usr/sbin /sbin\
   --clearenv \
-  --setenv PATH "/lean/bin" \
-  --setenv LAKE "/no" `# tries to invoke git otherwise` \
-  --setenv LEAN_PATH "$LEAN_PATH" \
+  --setenv PATH "/elan/bin:/bin" \
+  --setenv ELAN_HOME "/elan" \
   --unshare-user \
   --unshare-pid  \
   --unshare-net  \
   --unshare-uts  \
   --unshare-cgroup \
   --die-with-parent \
-  --chdir "/project" \
-  lean --server
+  --chdir "/project/" \
+  lake serve --
 )
