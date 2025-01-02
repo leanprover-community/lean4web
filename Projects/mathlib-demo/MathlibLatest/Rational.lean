6bf650fd87e099d4885881ac05b86ce8d20e457c
Martin Dvořák <martin.dvorak@matfyz.cz>
2025-01-02 14:08:18 +0100

    example(Rational): make spaces respect the order of evaluation

--- Projects/mathlib-demo/MathlibLatest/Rational.lean
+++ Projects/mathlib-demo/MathlibLatest/Rational.lean
@@ -11,5 +11,5 @@ example (h₁ : x < y) (h₂ : y + 3 < z + 10) : x + 37 < z + 44 := by
 
 -- let's prove that (x+y)^2=x^2+2*x*y+y^2
 
-example : (x + y)^2 = x^2 + 2 * x * y + y^2 := by
+example : (x + y) ^ 2 = x^2 + 2 * x * y + y^2 := by
   ring -- the `ring` tactic can do this automatically
