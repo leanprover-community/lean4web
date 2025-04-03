aea46d81503977d5f8cc83c71e18bcc3c3f994c4
Jon Eugster <eugster.jon@gmail.com>
2025-04-04 00:54:01 +0200

    caps fix 2

---
+++ Projects/Stable/lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «Stable» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Stable» where
+  -- add library configuration options here
