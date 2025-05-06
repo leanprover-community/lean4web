f1e60a5d1a926e0c3e821fcea726d3b0f1035aa8
Sebastian Ullrich <sebasti@nullri.ch>
2025-05-06 10:17:36 +0200

    fix: do not change project path in sandbox

--- server/bubblewrap.sh
+++ server/bubblewrap.sh
@@ -4,6 +4,7 @@
 ulimit -t 3600
 # NB: The RSS limit (ulimit -m) is not supported by modern linux!
 
+PROJECT="$(realpath "$1")"  # resolve symlinks
 LEAN_ROOT="$(cd $1 && lean --print-prefix)"
 LEAN_PATH="$(cd $1 && lake env printenv LEAN_PATH)"
 LEAN_SRC_PATH=$(cd $1 && lake env printenv LEAN_SRC_PATH)
@@ -14,7 +15,7 @@ GLIBC_PATH="$(nix-store --query "$(patchelf --print-interpreter "$LEAN_ROOT/bin/
 
 if true; then
   (exec bwrap\
-    --ro-bind "$1" /project \
+    --ro-bind "$PROJECT" "$PROJECT" \
     --ro-bind "$LEAN_ROOT" /lean \
     --ro-bind "$GLIBC_PATH" "$GLIBC_PATH" `# only dep of bin/lean` \
     --ro-bind /usr /usr \
@@ -38,7 +39,7 @@ if true; then
     --unshare-uts  \
     --unshare-cgroup \
     --die-with-parent \
-    --chdir "/project/" \
+    --chdir "$PROJECT" \
     lean --server
   )
 else
