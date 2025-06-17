33a1531208b542d3c34d8c09aff5a01b54e9e50a
Alexander Bentkamp <bentkamp@gmail.com>
2025-06-17 20:01:41 +0200

    add windows user agent test

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
     e2e: {
         baseUrl: "http://localhost:3000",
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            on('before:browser:launch', (browser, launchOptions) => {
+                if (browser.name === 'chromium' && process.env.CYPRESS_USER_AGENT_OS) {
+                    launchOptions.args.push(`--user-agent=\"Mozilla/5.0 (${process.env.CYPRESS_USER_AGENT_OS}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36\"`)
+                    return launchOptions
+                }
+            })
         },
     },
 });
