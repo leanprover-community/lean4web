3455712701ad701366aa43e851721d4308aecdb1
Joachim Breitner <mail@joachim-breitner.de>
2025-10-20 14:38:46 +0200

    Do not depend on webeditor
    upstream has dropped that already in 2799e4934d98d97c1d451035d003987dfcab2814

--- Projects/mathlib-stable/lakefile.lean
+++ Projects/mathlib-stable/lakefile.lean
@@ -4,9 +4,6 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"@"stable"
 
-require webeditor from git
-  "https://github.com/hhu-adam/lean4web-tools.git" @ "main"
-
 package mathlibStable {
   -- add package configuration options here
 }
