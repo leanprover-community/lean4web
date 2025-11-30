1bc2016431799537ff8edc9a5371b4696b374ef9
Jon Eugster <eugster.jon@gmail.com>
2025-11-30 23:53:43 +0100

    update workflow; use toml files

--- Projects/Stable/lakefile.lean
+++
@@ -1,9 +0,0 @@
-import Lake
-open Lake DSL
-
-package «Stable» where
-  -- add package configuration options here
-
-@[default_target]
-lean_lib «Stable» where
-  -- add library configuration options here
