2081e84b7751460baa17710a23d499079862641d
Joachim Breitner <mail@joachim-breitner.de>
2025-12-08 10:32:24 +0100

    Remove mathlib-v4.20.0-rc5

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -17,8 +17,6 @@ const lean4webConfig : LeanWebConfig = {
       "name": "Mathlib stable"},
     { "folder": "mathlib-v4.24.0",
       "name": "Mathlib v4.24.0"},
-    { "folder": "mathlib-v4.20.0-rc5",
-      "name": "Mathlib v4.20.0-rc5"},
     { "folder": "lean-nightly",
       "name": "Lean Nightly (without mathlib)"}
   ],
