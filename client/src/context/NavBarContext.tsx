8143c00d69275d6051aa5c98e69a3f6bcd8949bb
Robert J. Simmons <442315+robsimmons@users.noreply.github.com>
2026-05-26 12:15:39 -0400

    fix lint error (#3)
    Also could avoid a screen flash in some cases — not resetting these in the effect handler is an improvement, and worth the cost of two URLSearchParams constructors.

--- client/src/context/NavBarContext.tsx
+++ client/src/context/NavBarContext.tsx
@@ -10,23 +10,22 @@ type NavBarContextType = {
 const NavBarContext = createContext<NavBarContextType | undefined>(undefined);
 
 export const NavBarProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [requiresNavBar, setRequiresNavBar] = useState(0);
+  const [requiresNavBar, setRequiresNavBar] = useState(() => {
+    const params = new URLSearchParams(window.location.search);
+    const fromParam = params.get("from");
+    if (fromParam === "mathlib") return 1;
+    else if (fromParam === "lean") return 2;
+    return 0;
+  });
 
   const [hideNavBar, setHideNavBar] = useState<boolean>(() => {
+    const params = new URLSearchParams(window.location.search);
+    const hideParam = params.get("hide");
+    if (hideParam) return hideParam === "true";
+
     const stored = localStorage.getItem("hideNavBar");
     return stored ? stored === "true" : false;
   });
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const fromParam = params.get("from");
-    if (fromParam === "mathlib") setRequiresNavBar(1);
-    else if (fromParam === "lean") setRequiresNavBar(2);
-    else setRequiresNavBar(0);
-
-    const hideParam = params.get("hide");
-    if (hideParam) setHideNavBar(hideParam === "true");
-  }, []);
 
   useEffect(() => {
     localStorage.setItem("hideNavBar", hideNavBar.toString());
