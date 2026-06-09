a65bcb4ae88be1034312f41710a8d10637277c1b
Rob Simmons <rob@lean-fro.org>
2026-06-09 18:18:33 -0400

    update types

--- client/src/api/config-types.ts
+++ client/src/api/config-types.ts
@@ -13,4 +13,13 @@ export type LeanWebConfig = {
    * (example: `<><p>vat number: 000</p><>`)
    */
   impressum: JSX.Element | null
+  /** Base href for a comparator live deployment that encourages the verification of proofs from untrusted sources.
+   * If `null`, the "Can I Trust This Proof?" button will be hidden.
+   * (example: `"https://comparator.live.lean-lang.org/"`)
+   */
+  comparator: string | null
+  /** URLs to safelist so that refer links do not prompt a popup highlighting the "Can I Trust This Proof?" button.
+   * Ignored if `comparator` option is `null`.
+   */
+  comparatorSafeList: (string | RegExp)[] | null
 }
