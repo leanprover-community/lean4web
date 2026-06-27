d043136cfd65a89e2277036682569477629ecc51
Jon Eugster <eugster.jon@gmail.com>
2026-06-27 23:12:54 +0200

    fix

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'oxfmt'
 
 export default defineConfig({
-  ignorePatterns: ['dist/**', '*.min.js', '**/lake-manifest.json'],
+  ignorePatterns: ['**/lake-manifest.json'],
   printWidth: 80,
   semi: false,
   singleQuote: true,
